c7cc08b4dd8f0b61ac31e594a1a7b3cff7d89859
Sai Chandu_ Tammineni (tsaichandu@deloitte.com) <tsaichandu@your.hostname.com>
2022-05-09 19:18:41 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import React from 'react';
+import "./components/LoginComponent/Login.css"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./components/LoginComponent/Login";
 
 function App() {
   return <>
     <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        
+      </Routes>
+    </Router>
   <div className="App">App Component</div>
   </>
 }
