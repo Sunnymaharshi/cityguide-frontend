75b32e5d7a66acb88ccbef34e421369c74fd16a4
Sai Chandu_ Tammineni (tsaichandu@deloitte.com) <tsaichandu@your.hostname.com>
2022-05-09 18:32:19 +0530

    Login page created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import React from 'react';
+import "./components/LoginComponent/Login.css"
+import { BrowserRouter as Router, Routes, Route,Link} from "react-router-dom";
+import Login from "./components/LoginComponent/Login";
 
 function App() {
-  return <div className="App">App Component</div>;
+  // return <div className="App">App Component
+  // </div>;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="users" element={<Users />}>
+          <Route path="me" element={<OwnUserProfile />} />
+          <Route path=":id" element={<UserProfile />} />
+         </Route> */}
+      </Routes>
+    </Router>
+  );
+
+
 }
 
 export default App;
---
+++ src/components/LoginComponent/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+function Login(props) {
+    const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //const [gender, setGender] = useState('');
+  const [allRecords, setAllRecords] = useState([]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const newRecord = { email: email, password: password};
+    setAllRecords([...allRecords, newRecord]);
+    props.history.push({
+      pathname: '/temp',
+      state: allRecords,
+    });
+  };
+
+  return (
+    <div class="form">
+        
+        <form action="" onSubmit={submitForm}>
+        <h1><span class="log">LOGIN</span> <br></br>  <span class="to">to</span> <br></br><span class="city">CITY GUIDE</span></h1>
+
+        <div class="email">
+          <label htmlFor="email">Email</label>
+          <br></br>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div class="pass">
+          <label htmlFor="password">Password</label>
+          <br></br>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            autoComplete="off"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button class="button" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+    
+  )
+}
+
+export default Login
