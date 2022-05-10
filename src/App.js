6c431503d3cb3b72acb8b2c1bf212f310dcbf715
Reddy_ Tangasani Maharshi (tanreddy@deloitte.com) <tanreddy@your.hostname.com>
2022-05-10 10:06:00 +0530

    removed navbar comp in app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Signup from "./components/Signup/Signup";
-import Navbar from "./components/navbar/Navbar";
 import Login from "./components/login/Login";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
