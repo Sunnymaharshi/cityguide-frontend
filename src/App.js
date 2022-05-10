69603fa03aeb062f4bd3acbc4077cf67828595bb
siddhnigam-deloitte <svinayakamnigam@deloitte.com>
2022-05-10 11:27:37 +0530

    Added the SideBar component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Signup from "./components/Signup/Signup";
 import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/dashboard";
+import Sidebar from "./components/sidenav/Sidebar";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
-      <div className="App">App Component</div>
+      {/* <div className="App">App Component</div> */}
     </>
   );
 }
---
+++ src/components/sidenav/SidebarData.js
@@ -0,0 +1,31 @@
+import React from "react";
+export const SidebarData = [
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Bus Data",
+    link: "/busdata",
+  },
+  {
+    title: "Metro Map",
+    link: "/metromap",
+  },
+  {
+    title: "Top Restaurants",
+    link: "/restaurants",
+  },
+  {
+    title: "Top Attractions",
+    link: "/attractions",
+  },
+  {
+    title: "FAQ",
+    link: "/faq",
+  },
+  {
+    title: "Your Questions",
+    link: "/questions",
+  }
+];
