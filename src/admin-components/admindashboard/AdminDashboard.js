61f472ccaf152317227268313a68cec74c04f99d
Reddy_ Tangasani Maharshi (tanreddy@deloitte.com) <tanreddy@your.hostname.com>
2022-05-19 08:38:41 +0530

    added nav buttons for admin dashboard

--- src/admin-components/admindashboard/AdminDashboard.js
+++ src/admin-components/admindashboard/AdminDashboard.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, NavLink } from "react-router-dom";
 import City from "../CRUDcity/City";
 import UserContext from "../../context/user/user.context";
 import "./AdminDashboard.css";
@@ -10,25 +10,28 @@ function AdminDashboard() {
 
   useEffect(() => {
     checkUserLogin();
+    // eslint-disable-next-line
   }, []);
   return (
     <div className="admin-comp">
       {user.role === "Admin" && (
         <div className="admin-content">
-          {/* <div className="city-comp">
-          <City />
-          </div> */}
-          {/* <div className="rest-comp">
-          <Restaurant/>
+          <div className="admin-btns">
+            <NavLink to="/admin/city" className="admin-btn">
+              Cities
+            </NavLink>
+            <NavLink to="/admin/restaurant" className="admin-btn">
+              Restaurants
+            </NavLink>
+            <NavLink to="/admin/attraction" className="admin-btn">
+              Attractions
+            </NavLink>
           </div>
-          <div className="attr-comp">
-          <Attraction/>
-          </div> */}
           <Routes>
-            <Route path="/" element={<Navigate replace to="city" />}/>
-            <Route path="city" element={<City/>} />
-            <Route path="restaurant" element={<Restaurant/>} />
-            <Route path="attraction" element={<Attraction/>} />
+            <Route path="/" element={<Navigate replace to="city" />} />
+            <Route path="city" element={<City />} />
+            <Route path="restaurant" element={<Restaurant />} />
+            <Route path="attraction" element={<Attraction />} />
           </Routes>
         </div>
       )}
