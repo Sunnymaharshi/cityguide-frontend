ca6240646f784f79339d386c0627649c50cc38db
Reddy_ Tangasani Maharshi (tanreddy@deloitte.com) <tanreddy@your.hostname.com>
2022-05-19 17:27:57 +0530

    added key property for tables and removed logs

--- src/admin-components/CRUDcity/City.js
+++ src/admin-components/CRUDcity/City.js
@@ -1,42 +1,41 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./City.css";
-import { deleteCity, getCity, postCity, updateCity } from "../../services/admin/city.service";
+import {
+  deleteCity,
+  getCity,
+  postCity,
+  updateCity,
+} from "../../services/admin/city.service";
 
 function City() {
   const [city_name, setCityName] = useState("");
   const [city_desc, setDesc] = useState("");
   const [successMsg, setSuccessMsg] = useState(null);
-  const [city, setCity]=useState([]);
-  console.log(city_name);
-  console.log(city_desc);
-  
+  const [city, setCity] = useState([]);
+
   useEffect(() => {
     getAllCities();
-     
-}, []);
+  }, []);
 
-const getAllCities = async (event) =>{
+  const getAllCities = async (event) => {
     getCity()
       .then((res) => {
-          setCity(res.data);
-          console.log(city);
-        }
-      )
+        setCity(res.data);
+      })
       .catch((err) => {
         console.log("Error", err);
       });
-}
+  };
 
   const sendDataToAPI = async (event) => {
     event.preventDefault();
-    postCity(city_name,city_desc)
+    postCity(city_name, city_desc)
       .then(function (response) {
-        if (response.data.city_name === city_name){
-        setSuccessMsg("Successfully Added!");
-        setCityName("");
-        setDesc("");
-        getAllCities();
-        console.log(response);
+        if (response.data.city_name === city_name) {
+          setSuccessMsg("Successfully Added!");
+          setCityName("");
+          setDesc("");
+          getAllCities();
         }
       })
       .catch(function (error) {
@@ -46,14 +45,13 @@ const getAllCities = async (event) =>{
 
   const updateDataToAPI = async (event) => {
     event.preventDefault();
-    updateCity(city_name,city_desc)
+    updateCity(city_name, city_desc)
       .then(function (response) {
-        if (response.data.city_name === city_name){
-        setSuccessMsg("Successfully Updated!");
-        setCityName("");
-        setDesc("");
-        getAllCities();
-        console.log(response);
+        if (response.data.city_name === city_name) {
+          setSuccessMsg("Successfully Updated!");
+          setCityName("");
+          setDesc("");
+          getAllCities();
         }
       })
       .catch(function (error) {
@@ -65,12 +63,11 @@ const getAllCities = async (event) =>{
     event.preventDefault();
     deleteCity(city_name)
       .then(function (response) {
-        if (response.data === "Deleted!"){
-        setSuccessMsg("Successfully Deleted!");
-        setCityName("");
-        setDesc("");
-        getAllCities();
-        console.log(response.data);
+        if (response.data === "Deleted!") {
+          setSuccessMsg("Successfully Deleted!");
+          setCityName("");
+          setDesc("");
+          getAllCities();
         }
       })
       .catch(function (error) {
@@ -80,92 +77,92 @@ const getAllCities = async (event) =>{
 
   return (
     <div className="city-div">
-    <div className="city-form">
-      
-      <form>
-        <h1 className="city-op">City Operations</h1>
+      <div className="city-form">
+        <form>
+          <h1 className="city-op">City Operations</h1>
 
-        <div className="form-group">
-          <label htmlFor="cityname">City Name</label>
-          <input
-            onChange={(e) => setCityName(e.target.value)}
-            type="text"
-            name="cityname"
-            placeholder="City Name"
-            id="cityname"
-            value={city_name}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="citydesc">City Description</label>
-          <input
-            onChange={(e) => setDesc(e.target.value)}
-            type="text"
-            name="citydesc"
-            placeholder="City Description"
-            id="citydesc"
-            value={city_desc}
-            className="form-control"
-          />
-        </div>
+          <div className="form-group">
+            <label htmlFor="cityname">City Name</label>
+            <input
+              onChange={(e) => setCityName(e.target.value)}
+              type="text"
+              name="cityname"
+              placeholder="City Name"
+              id="cityname"
+              value={city_name}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="citydesc">City Description</label>
+            <input
+              onChange={(e) => setDesc(e.target.value)}
+              type="text"
+              name="citydesc"
+              placeholder="City Description"
+              id="citydesc"
+              value={city_desc}
+              className="form-control"
+            />
+          </div>
 
-        <div className="btn-main">
-          <div className="add-button">
-            <button
-              type="submit"
-              className="delete-btn"
-              disabled={!city_name || !city_desc}
-              onClick={sendDataToAPI}
-            >
-              Add
-            </button>
+          <div className="btn-main">
+            <div className="add-button">
+              <button
+                type="submit"
+                className="delete-btn"
+                disabled={!city_name || !city_desc}
+                onClick={sendDataToAPI}
+              >
+                Add
+              </button>
+            </div>
+            <div className="update-btn">
+              <button
+                type="submit"
+                className="delete-btn"
+                disabled={!city_name || !city_desc}
+                onClick={updateDataToAPI}
+              >
+                Update
+              </button>
+            </div>
+            <div className="">
+              <button
+                type="submit"
+                className="delete-btn"
+                disabled={!city_name}
+                onClick={onDelete}
+              >
+                Delete
+              </button>
+            </div>
           </div>
-          <div className="update-btn">
-            <button
-              type="submit"
-              className="delete-btn"
-              disabled={!city_name || !city_desc}
-              onClick={updateDataToAPI}
-            >
-              Update
-            </button>
-          </div>
-          <div className="">
-            <button type="submit"
-             className="delete-btn"
-             disabled={!city_name}
-              onClick={onDelete}>
-              Delete
-            </button>
-          </div>
-        </div>
-        <p>
-          <b>{successMsg}</b>
-        </p>
-      </form>
+          <p>
+            <b>{successMsg}</b>
+          </p>
+        </form>
+      </div>
+      <div className="city-table">
+        <h2 className="city-det">City Details</h2>
+        <table id="citytable">
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+            </tr>
+            {city.map((c) => {
+              return (
+                <tr key={c.city_name}>
+                  <td>{c.city_name}</td>
+                  <td>{c.city_desc}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div className='city-table'>
-    <h2 className='city-det'>City Details</h2>
-<table id="citytable">
-  <tbody>
-      <tr>
-          <th>Name</th>
-          <th>Description</th>
-      </tr>
-  {city.map((c) => {
-return(
-  <tr>
-      <td>{c.city_name}</td>
-      <td>{c.city_desc}</td>
-  </tr>
-)
-})}
-</tbody>
-</table>
-
-</div>
-</div>
   );
 }
 
