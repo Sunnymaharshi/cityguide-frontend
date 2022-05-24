5ac56fd58053827858870295e4ee109016ebc55f
Roy_ Surabhi (surabroy@deloitte.com) <surabroy@your.hostname.com>
2022-05-24 19:51:08 +0530

    Made changes in Admin Forms UI

---
+++ src/services/admin/bus.service.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { BASE_URL } from "../../common/data";
+export const postBus = async (city_name,busmap_img)=>{
+    const userDetails = JSON.parse(localStorage.getItem("user"));
+    return await axios
+      .post(
+        BASE_URL+"/addbus",
+        {
+          city_name,
+          busmap_img
+
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + userDetails.token,
+          },
+        }
+      )
+}
+
+export const uploadFile = (city_name,selectedFile) => {
+
+
+    console.log(selectedFile);
+    const formData = new FormData();
+      formData.append('image',selectedFile)
+    const userDetails = JSON.parse(localStorage.getItem("user"));
+      return axios
+        .post(BASE_URL+`/imageUpload/${city_name}`, formData,
+  
+        {
+          headers: {
+            Authorization: "Bearer " + userDetails.token,
+            'content-type': 'multipart/form-data'
+          }
+        })
+    }
+export const getimgurl=(city_name,selectedFile)=>{
+   
+    return axios.get(BASE_URL+`/geturl/${city_name}/${selectedFile.name}`)}
