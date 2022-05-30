7b43f3fda67150c5e2ab6fb52d23135ead8ae73f
Sai Chandu_ Tammineni (tsaichandu@deloitte.com) <tsaichandu@your.hostname.com>
2022-05-30 13:33:14 +0530

    added metro page and carousel in home

---
+++ src/components/accordion/Accordion.js
@@ -0,0 +1,41 @@
+import { motion, AnimatePresence } from "framer-motion";
+
+
+const Accordion = ({ i, expanded, setExpanded ,line}) => {
+    const isOpen = i === expanded;
+  
+    // By using `AnimatePresence` to mount and unmount the contents, we can animate
+    // them in and out while also only rendering the contents of open accordions
+    return (
+      <>
+        <motion.header
+          initial={false}
+          animate={{ backgroundColor: isOpen ? "#FF0088" : "#0055FF" }}
+          onClick={() => setExpanded(isOpen ? false : i)}
+        >
+            <h3>{line.title}</h3>
+        </motion.header>
+        <AnimatePresence initial={false}>
+          {isOpen && (
+            <motion.section
+              key="content"
+              initial="collapsed"
+              animate="open"
+              exit="collapsed"
+              variants={{
+                open: { opacity: 1, height: "auto" },
+                collapsed: { opacity: 0, height: 0 }
+              }}
+              transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+            >
+
+              <img className="metro-line-data" src={line.img} alt="no data"/>
+
+            </motion.section>
+          )}
+        </AnimatePresence>
+      </>
+    );
+  };
+
+  export default Accordion;
