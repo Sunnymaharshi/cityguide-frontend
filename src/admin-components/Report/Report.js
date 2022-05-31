02e4bf79c7f894be8f1209102e92078c49afa7e8
Roy_ Surabhi (surabroy@deloitte.com) <surabroy@your.hostname.com>
2022-05-31 12:00:12 +0530

    Made changes in Report form ADMIN

--- src/admin-components/Report/Report.js
+++ src/admin-components/Report/Report.js
@@ -14,11 +14,11 @@ function Report() {
 
 const validateDelete = (e) =>{
   e.preventDefault();
-  if(report_id && report_type && report_type_id){
+  if(report_type && report_type_id){
   onDelete(e);
   }
   else{
-      toast.error("Fill report id, type and type id!");
+      toast.error("Fill report type and type id!");
   }
  }
  const validateDelReport = (e) =>{
@@ -41,10 +41,10 @@ const validateDelete = (e) =>{
   };
   const onDelete = (e) => {
     e.preventDefault();
-    deleteReport(report_id, report_type, report_type_id)
+    deleteReport( report_type, report_type_id)
       .then((res) => {
         toast.success();
-        setReportId("");
+        // setReportId("");
         setReportType("");
         setReportTypeId("");
         getAllReports();
@@ -72,6 +72,37 @@ const validateDelete = (e) =>{
       <div className="report-form">
         <form>
           <h2 className="city-op">Report Operations</h2>
+          <div className="form-row">
+             <div className="form-group col">
+            <label htmlFor="reportid">Report Id</label>
+            {/* <div className="img-form"> */}
+              {/* <div> */}
+            <input
+              onChange={(e) => {setReportId(e.target.value);}}
+              type="text"
+              name="reportid"
+              placeholder="Report Id"
+              id="reportid"
+              value={report_id}
+              className="form-control"
+            />
+            {/* </div> */}
+             </div>
+             <div className="form-group col">
+          <div className="btn-main">
+              <div className="update-btn">
+            <button
+              type="submit"
+              style={{marginTop:"1.5rem"}}
+              className="delete-btn"
+              onClick={validateDelReport}
+            >
+              Delete Report
+            </button>
+         </div>
+         </div>
+             </div>
+          </div>
           <div className="form-group">
             <label htmlFor="reporttype">Report Type</label>
             <input
@@ -97,18 +128,7 @@ const validateDelete = (e) =>{
               className="form-control"
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="reportid">Report Id</label>
-            <input
-              onChange={(e) => {setReportId(e.target.value);}}
-              type="text"
-              name="reportid"
-              placeholder="Report Id"
-              id="reportid"
-              value={report_id}
-              className="form-control"
-            />
-          </div>
+
           <div className="btn-main">
               <div className="update-btn">
             <button
@@ -120,20 +140,10 @@ const validateDelete = (e) =>{
               Delete Data
             </button>
             </div>
-            <div>
-            <button
-              style={{ marginTop: "0.5rem" }}
-              type="submit"
-              className="delete-btn"
-              
-              onClick={validateDelReport}
-            >
-              Delete Report
-            </button>
-            </div>
           </div>
         </form>
       </div>
+
       <div className="city-table">
         <table id="citytable">
           <tbody>
@@ -157,6 +167,7 @@ const validateDelete = (e) =>{
         </table>
       </div>
     </div>
+
   );
 }
 
