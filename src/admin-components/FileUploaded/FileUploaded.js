7d6177a0554891379bb74d6b095a74243122f91d
Reddy_ Tangasani Maharshi (tanreddy@deloitte.com) <tanreddy@your.hostname.com>
2022-06-05 22:22:07 +0530

    resolved warnings

--- src/admin-components/FileUploaded/FileUploaded.js
+++ src/admin-components/FileUploaded/FileUploaded.js
@@ -1,21 +1,19 @@
-import React,{useRef} from 'react'
+import React from "react";
 
+export const FileUploaded = ({ onFileSelect }) => {
+  const handleFileInput = (e) => {
+    onFileSelect(e.target.files[0]);
+  };
 
-export const FileUploaded = ({onFileSelect}) => {
-        const fileInput = useRef(null)
-    
-        const handleFileInput = (e) => {
-        onFileSelect(e.target.files[0]);
-        }
-    
-        return (
-        
-            <div className="file-uploader">
-                <label htmlFor="chooseimage">Choose Image</label>
-                <input type="file" name="chooseimage"onChange={handleFileInput} className="form-control"></input>
-            </div>
-            
-        )
-    }
-
-
+  return (
+    <div className="file-uploader">
+      <label htmlFor="chooseimage">Choose Image</label>
+      <input
+        type="file"
+        name="chooseimage"
+        onChange={handleFileInput}
+        className="form-control"
+      ></input>
+    </div>
+  );
+};
