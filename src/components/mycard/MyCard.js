c182a54b30a42fe321fbf54217cec377acd99aa5
Reddy_ Tangasani Maharshi (tanreddy@deloitte.com) <tanreddy@your.hostname.com>
2022-06-06 21:30:36 +0530

    added loading for card

--- src/components/mycard/MyCard.js
+++ src/components/mycard/MyCard.js
@@ -1,4 +1,10 @@
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Skeleton,
+  Typography,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { getImages } from "../../services/dashboard/dashboard.service";
 import { IMG_ATTR_TYPE, IMG_RES_TYPE } from "../../common/data";
@@ -6,6 +12,7 @@ import "./MyCard.css";
 import { toast } from "react-toastify";
 const MyCard = ({ details }) => {
   const [img_url, setImg_url] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (details.res_id) {
@@ -13,33 +20,40 @@ const MyCard = ({ details }) => {
         .then((res) => {
           if (res.data.length > 0) {
             setImg_url(res.data[0].img_url);
+            setLoading(false);
           }
         })
         .catch((err) => {
           toast.error(err.response.data, { autoClose: 5000 });
+          setLoading(false);
         });
     } else {
       getImages(IMG_ATTR_TYPE, details.attr_id)
         .then((res) => {
           if (res.data.length > 0) {
             setImg_url(res.data[0].img_url);
+            setLoading(false);
           }
         })
         .catch((err) => {
           toast.error(err.response.data, { autoClose: 5000 });
+          setLoading(false);
         });
     }
     // eslint-disable-next-line
   }, []);
   return (
     <Card sx={{ maxWidth: 445 }} className="mycard-comp">
-      <CardMedia
-        component="img"
-        height="340"
-        image={img_url}
-        alt="attraction"
-        className="card-image"
-      />
+      {!loading && (
+        <CardMedia
+          component="img"
+          height="340"
+          image={img_url}
+          alt="attraction"
+          className="card-image"
+        />
+      )}
+      {loading && <Skeleton height={300} />}
       <CardContent>
         <Typography
           gutterBottom
